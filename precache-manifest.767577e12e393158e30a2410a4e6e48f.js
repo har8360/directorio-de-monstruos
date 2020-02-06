bc8e6ed02c1d2cf61aada00fbdbd7f6a8ec8ee2b
har8360 <har8360@hotmail.com>
2020-02-05 23:39:55 -0600

    Updates

---
+++ precache-manifest.767577e12e393158e30a2410a4e6e48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140e53f7e3f9783f431ea2d9321ebb45",
+    "url": "/directorio-de-monstruos/index.html"
+  },
+  {
+    "revision": "fb87c1e4403d1294966a",
+    "url": "/directorio-de-monstruos/static/css/main.fadff892.chunk.css"
+  },
+  {
+    "revision": "3f5e71ab7da0d1c2c04f",
+    "url": "/directorio-de-monstruos/static/js/2.5685baa6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/directorio-de-monstruos/static/js/2.5685baa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb87c1e4403d1294966a",
+    "url": "/directorio-de-monstruos/static/js/main.6ce8768f.chunk.js"
+  },
+  {
+    "revision": "b0144a08ccba7cc0b93d",
+    "url": "/directorio-de-monstruos/static/js/runtime-main.72b8ad48.js"
+  }
+]);
